da4c04b11844d2f71044e5d58fa0cc0363249fe2
Nikko <nikkonuriel@gmail.com>
2024-09-18 22:16:51 +0800

    Initial commit for Microservices API Exercise

---
+++ order-service/index.js
@@ -0,0 +1,108 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+
+app.use(express.json());
+
+let orders = [];
+
+// Route for Creating an Order
+app.post('/orders', async (req, res) => {
+    console.log(req.body);
+    try {
+        const { customerId, productId } = req.body;
+
+        const customerResponse = await axios.get(`http://localhost:3002/customers/${customerId}`);
+        const customerName = customerResponse.data.name;
+        const customerAge = customerResponse.data.age;
+
+        const productResponse = await axios.get(`http://localhost:3001/products/${productId}`);
+        const productName = productResponse.data.name;
+        const productPrice = productResponse.data.price;
+
+        const order = { orderId: Date.now(), customerId, productId, customerName, productName, customerAge, productPrice };
+        orders.push(order);
+        res.status(200).send(order);
+
+    } catch (error) {
+        res.status(400).send('Invalid Customer or Product');
+    }
+})
+
+// Route for Retrieving Orders by ID
+app.get('/orders/:orderId', (req, res) => {
+    try {
+        const order = orders.find(o => o.orderId == req.params.orderId);
+
+        if (!order) {
+            return res.status(400).json({ message: 'Order Does Not Exist'});
+        }
+
+        res.send(order);
+
+    } catch (error) {
+        res.status(500).json({ message: 'Could Not Retrieve Order' });
+    }
+})
+
+// Route for Retrieving all Orders 
+app.get('/orders', (req, res) => {
+    try {
+        if (!orders) {
+            return res.status(400).json({ message: 'Orders Do Not Exist' });
+        }
+
+        res.json(orders);
+
+    } catch (error) {
+        res.status(500).json({ message: 'Could Not Retrieve Orders' });
+    }
+})
+
+// Route for Updating an Order by Id
+app.put('/orders/:orderId', async (req, res) => {
+    try {
+        const order = orders.find(o => o.orderId == req.params.orderId);
+
+        if (!order) {
+            return res.status(400).json({ message: 'Order Does Not Exist'});
+        }
+
+        const { customerId, productId } = req.body;
+
+        if (productId && productId !== order.productId) {
+            const productResponse = await axios.get(`http://localhost:3001/products/${productId}`);
+            order.productId = productId;
+            order.productName = productResponse.data.name;
+            order.productPrice = productResponse.data.price; 
+        }
+        
+        if (customerId && customerId !== order.customerId) {
+            const customerResponse = await axios.get(`http://localhost:3002/customers/${customerId}`);
+            order.customerId = customerId;
+            order.customerName = customerResponse.data.name;
+            order.customerAge = customerResponse.data.age; 
+        }
+
+        Object.assign(order, req.body);
+        res.status(200).json({ message: 'Successfully Updated Order' });
+
+    } catch (error) {
+        res.status(500).json({ message: ''})
+    }
+})
+
+// Route for Deleting an Order by Id
+app.delete('/orders/:orderId', (req, res) => {
+    try {
+        orders = orders.filter(o => o.orderId != req.params.orderId);
+        res.status(200).json({ message: 'Successfully Deleted Order' });
+    } catch (error) {
+        res.status(500).json({ message: 'Could Not Delete Order' });
+    }
+})
+
+
+app.listen(3003, () => {
+    console.log('Order service is running on port 3003');
+})
