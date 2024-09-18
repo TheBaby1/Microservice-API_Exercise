e0e84639097a1bce1f1e6a7a76ce8044da0ae939
srx26 <91110292+srx26@users.noreply.github.com>
2024-09-18 22:56:17 +0800

    Add files via upload

---
+++ customer-service/index.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let customers = [];
+
+// Route for Creating a Customer
+app.post('/customers', (req, res) => {
+    try {
+        const customerId = Date.now();
+        const { name } = req.body;
+
+        if (!name) {
+            return res.status(400).json({ message: 'Name is required!'});
+        }
+
+        const customer = {
+            customerId,
+            name
+        };
+
+        customers.push(customer);
+        res.status(201).json({ message: 'Successfully added customer!'});
+
+    } catch (error) {
+        res.status(500).json({ message: 'Could Not Create Customer'});
+    }
+})
+
+// (Optional Route) Route for Retrieving all customers
+app.get('/customers', (req, res) => {
+    try {
+        if (!customers) {
+            return res.status(400).json({ message: 'Customers Do Not Exist'});
+        }
+
+        res.json(customers);
+
+    } catch (error) {
+        res.status(500).json({ message: 'Could Not Retrieve Customers'});
+    }
+})
+
+
+// Route for Retrieving Customer Details by ID
+app.get('/customers/:customerId', (req, res) => {
+    try {
+        const customer = customers.find(p => p.customerId == req.params.customerId);
+
+        if (!customer) {
+            return res.status(404).json({ message: 'Customer not Found'});
+        }
+
+        res.send(customer);
+
+    } catch (error) {
+        res.status(500).json({ message: 'Could Not Retrieve Customer'});
+    }
+})
+
+// Route for Updating a Customer by ID
+app.put('/customers/:customerId', (req, res) => {
+    try {
+        const customer = customers.find(p => p.customerId == req.params.customerId);
+
+        if (!customer) {
+            return res.status(400).json({ message: 'Customer not Found' });
+        }
+
+        Object.assign(customer, req.body);
+        res.status(200).json({ message: 'Successfully Updated Customer' });
+
+    } catch (error) {
+        res.status(500).json({ message: 'Could Not Update Customer' });
+    }
+})
+
+// Route for Deleting a Customer by ID
+app.delete('/customers/:customerId', (req, res) => {
+    try {
+        customers = customers.filter(p => p.customerId != req.params.customerId);
+        res.status(200).json({ message: 'Successfully Deleted Customer'});
+    } catch (error) {
+        res.status(500).json({ message: 'Could Not Delete Customer'});
+    }
+})
+
+app.listen(3002, () => {
+    console.log('Customer Service is running on port 3002');
+})
